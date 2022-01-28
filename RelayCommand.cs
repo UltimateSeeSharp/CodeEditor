c826cff39640a06a2b07997074815bc48c48733b
UltimateSeeSharp <91130013+UltimateSeeSharp@users.noreply.github.com>
2022-01-28 12:11:26 +0100

    Create RelayCommand.cs

---
+++ RelayCommand.cs
@@ -0,0 +1,96 @@
+internal class RelayCommand<T> : ICommand
+{
+    private readonly Action<T> mExecute = null;
+    private readonly Predicate<T> mCanExecute = null;
+
+    public RelayCommand(Action<T> execute)
+      : this(execute, null)
+    {
+    }
+
+    public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+    {
+        if (execute == null)
+            throw new ArgumentNullException("execute");
+
+        this.mExecute = execute;
+        this.mCanExecute = canExecute;
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add
+        {
+            if (this.mCanExecute != null)
+                CommandManager.RequerySuggested += value;
+        }
+
+        remove
+        {
+            if (this.mCanExecute != null)
+                CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    [DebuggerStepThrough]
+    public bool CanExecute(object parameter)
+    {
+        return this.mCanExecute == null ? true : this.mCanExecute((T)parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+        this.mExecute((T)parameter);
+    }
+}
+
+internal class RelayCommand : ICommand
+{
+    private readonly Action mExecute;
+    private readonly Func<bool> mCanExecute;
+
+    public RelayCommand(Action execute)
+      : this(execute, null)
+    {
+    }
+
+    public RelayCommand(RelayCommand inputRC)
+      : this(inputRC.mExecute, inputRC.mCanExecute)
+    {
+    }
+
+    public RelayCommand(Action execute, Func<bool> canExecute)
+    {
+        if (execute == null)
+            throw new ArgumentNullException("execute");
+
+        this.mExecute = execute;
+        this.mCanExecute = canExecute;
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add
+        {
+            if (this.mCanExecute != null)
+                CommandManager.RequerySuggested += value;
+        }
+
+        remove
+        {
+            if (this.mCanExecute != null)
+                CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    [DebuggerStepThrough]
+    public bool CanExecute(object parameter)
+    {
+        return this.mCanExecute == null ? true : this.mCanExecute();
+    }
+
+    public void Execute(object parameter)
+    {
+        this.mExecute();
+    }
+}
