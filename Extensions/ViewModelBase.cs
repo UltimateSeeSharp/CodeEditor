cd40cd30e55231ae887e26e0cc3e21745ad15396
UltimateSeeSharp <91130013+UltimateSeeSharp@users.noreply.github.com>
2022-01-28 12:31:58 +0100

    Update ViewModelBase.cs

--- Extensions/ViewModelBase.cs
+++ Extensions/ViewModelBase.cs
@@ -1,3 +1,9 @@
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Placeholder;
+
 public class ViewModelBase : INotifyPropertyChanged
 {
     /// Standard implementation of <seealso cref="INotifyPropertyChanged"/>.
