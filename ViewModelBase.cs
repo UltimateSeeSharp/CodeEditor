2feb4390f45d94548208809a925b46b74583a259
UltimateSeeSharp <91130013+UltimateSeeSharp@users.noreply.github.com>
2022-01-28 12:08:22 +0100

    Create ViewModelBase.cs

---
+++ ViewModelBase.cs
@@ -0,0 +1,33 @@
+public class ViewModelBase : INotifyPropertyChanged
+{
+    /// Standard implementation of <seealso cref="INotifyPropertyChanged"/>.
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    /// Tell bound controls (via WPF binding) to refresh their display.
+    /// Sample call: this.NotifyPropertyChanged(() => this.IsSelected);
+    /// where 'this' is derived from <seealso cref="ViewModelBase"/>
+    /// and IsSelected is a property.
+    public void RaisePropertyChanged<TProperty>(Expression<Func<TProperty>> property)
+    {
+        var lambda = (LambdaExpression)property;
+        MemberExpression memberExpression;
+
+        if (lambda.Body is UnaryExpression)
+        {
+            var unaryExpression = (UnaryExpression)lambda.Body;
+            memberExpression = (MemberExpression)unaryExpression.Operand;
+        }
+        else
+            memberExpression = (MemberExpression)lambda.Body;
+
+        this.RaisePropertyChanged(memberExpression.Member.Name);
+    }
+
+    /// Tell bound controls (via WPF binding) to refresh their display.
+    /// Standard implementation through <seealso cref="INotifyPropertyChanged"/>.
+    protected virtual void RaisePropertyChanged(string propertyName)
+    {
+        if (this.PropertyChanged != null)
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
